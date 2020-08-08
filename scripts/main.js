868ba91f6b80802605b4890c041ae7c55062fae9
Luther Calvin Riggs <luthercalvinriggsllc@gmail.com>
2020-08-07 20:13:55 -0400

    completed assignment funtionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
+const canvas = document.getElementsByTagName('BODY')[0];
+console.log(canvas);
+
 function createGrid() {
-    let gridSquare = document.createElement('div');
-};
+    // for (let rowNum = 0; rowNum < 16; rowNum++){
+        const gridRow = document.createElement('div');
+        gridRow.className = 'grid-row';
+        console.log(gridRow);
+
+        // gridRow.id = `grid-row-${rowNum}`;
+        
+        // for (let colNum = 0; colNum < 16; colNum++) {
+        //     const gridColumn = document.createElement('div');
+        //     gridColumn.className = 'grid-square';
+        //     gridColumn.id = `grid-${rowNum}-${colNum}`);
+
+        //     gridRow.appendChild(gridColumn);            
+        // }
+        canvas.appendChild(gridRow);
+    // }
+}
+
+createGrid();
